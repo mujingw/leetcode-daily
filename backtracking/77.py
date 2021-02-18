0a000c63da8917a225dda499e38234196c35266d
mujingw <mqw5130@gmail.com>
2021-02-17 21:09:05 -0500

    [LC-77]

---
+++ backtracking/77.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def dfs(res, curr, start, n, k):
+            if len(curr) == k:
+                res.append(curr[:])
+
+                return
+
+            for i in range(start, n + 1):
+                curr.append(i)
+                dfs(res, curr, i + 1, n, k)
+                curr.pop()
+
+        res = []
+        dfs(res, [], 1, n, k)
+
+        return res
