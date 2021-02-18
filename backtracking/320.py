9017e778312b7b9acb15be1b4f6cb8a4e401022f
mujingw <mqw5130@gmail.com>
2021-02-18 17:40:36 -0500

    [LC-320]

---
+++ backtracking/320.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generateAbbreviations(self, word: str) -> List[str]:
+        def dfs(res, curr, word):
+            if not word:
+                res.append(curr)
+
+                return
+
+            dfs(res, curr + word[0], word[1:])
+
+            if curr and curr[-1].isdigit():
+                dfs(res, curr[:-1] + str(int(curr[-1]) + 1), word[1:])
+            else:
+                dfs(res, curr + "1", word[1:])
+
+        res = []
+        dfs(res, "", word)
+
+        return res
