63e158340eada836e1144a092d5c7699b761de0f
mujingw <mqw5130@gmail.com>
2021-02-19 18:58:36 -0500

    [LC-131]

---
+++ backtracking/131.py
@@ -0,0 +1,18 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def dfs(res, curr, s):
+            if s:
+                for i in range(1, len(s) + 1):
+                    prefix = s[:i]
+
+                    if prefix == prefix[::-1]:
+                        curr.append(prefix)
+                        dfs(res, curr, s[i:])
+                        curr.pop()
+            else:
+                res.append(curr[:])
+
+        res = []
+        dfs(res, [], s)
+
+        return res
