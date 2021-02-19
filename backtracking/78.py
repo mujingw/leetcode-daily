770630c87694c135b5d8a06ea8383fd807d473ea
mujingw <mqw5130@gmail.com>
2021-02-18 20:15:17 -0500

    [LC-78]

---
+++ backtracking/78.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def dfs(res, curr, nums, start):
+            res.append(curr[:])
+
+            for i in range(start, len(nums)):
+                curr.append(nums[i])
+                dfs(res, curr, nums, i + 1)
+                curr.pop()
+
+        res = []
+        dfs(res, [], nums, 0)
+
+        return res
