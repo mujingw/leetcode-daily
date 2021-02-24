fc50195e293a4ef65711d67bfbe148eca4e3c080
mujingw <mqw5130@gmail.com>
2021-02-24 01:02:43 -0500

    [LC-1371]

--- two_pointers/pre_sum/1371.py
+++ two_pointers/pre_sum/1371.py
@@ -1,30 +1,15 @@
-from collections import defaultdict
-
-
 class Solution:
     def findTheLongestSubstring(self, s: str) -> int:
-        def get_mask(window):
-            res = 0
-            lookup = {"a": 16, "e": 8, "i": 4, "o": 2, "u": 1}
-
-            for ch, count in window.items():
-                if ch in "aeiou" and count % 2 != 0:
-                    res += lookup[ch]
-
-            return res
-
-        pre = defaultdict(list)
-        pre[0].append(-1)
-        res = 0
-        window = defaultdict(int)
+        vowels = {"a": 16, "e": 8, "i": 4, "o": 2, "u": 1}
+        pre, res, vowels_xor = {0: -1}, 0, 0
 
         for i, ch in enumerate(s):
-            window[ch] += 1
-            mask = get_mask(window)
+            if ch in vowels:
+                vowels_xor ^= vowels[ch]
 
-            if mask in pre:
-                res = max(res, i - pre[mask][0])
+            if vowels_xor not in pre:
+                pre[vowels_xor] = i
 
-            pre[mask].append(i)
+            res = max(res, i - pre[vowels_xor])
 
         return res
