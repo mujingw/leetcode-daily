3a9117ac69e9a7380b42f649eb7f264162b415c4
mujingw <mqw5130@gmail.com>
2021-03-02 18:12:23 -0500

    [LC-1654]

---
+++ breadth_first_search/1654.py
@@ -0,0 +1,26 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def minimumJumps(self, forbidden: List[int], a: int, b: int, x: int) -> int:
+        q = collections.deque([(0, "none", 0)])
+        seen = set([0])
+        forbidden = set(forbidden)
+        max_pos = max(x, max(forbidden)) + a + b
+
+        while q:
+            curr, incoming_dir, jumps = q.popleft()
+
+            if curr == x:
+                return jumps
+
+            if curr + a not in seen and curr + a not in forbidden and curr + a <= max_pos:
+                q.append((curr + a, "forward", jumps + 1))
+                seen.add(curr + a)
+
+            if incoming_dir != "backward":
+                if curr - b >= 0 and curr - b not in forbidden:
+                    q.append((curr - b, "backward", jumps + 1))
+
+        return -1
