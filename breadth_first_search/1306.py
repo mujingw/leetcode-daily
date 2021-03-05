9655a8ff6c01670f269c16f9b8cf95f2d6eb8cfb
mujingw <mqw5130@gmail.com>
2021-03-05 17:30:16 -0500

    [LC-1306]

---
+++ breadth_first_search/1306.py
@@ -0,0 +1,29 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        q = collections.deque([start])
+        seen = set([start])
+        N = len(arr)
+
+        while q:
+            curr = q.popleft()
+
+            if arr[curr] == 0:
+                return True
+
+            next_pos = curr + arr[curr]
+
+            if next_pos not in seen and 0 <= next_pos < N:
+                seen.add(next_pos)
+                q.append(next_pos)
+
+            next_pos = curr - arr[curr]
+
+            if next_pos not in seen and 0 <= next_pos < N:
+                seen.add(next_pos)
+                q.append(next_pos)
+
+        return False
