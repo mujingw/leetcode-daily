16c0cf2352bddbc2a855ef667f419a17d8c99fb6
mujingw <mqw5130@gmail.com>
2021-03-05 18:25:05 -0500

    [LC-127]

---
+++ breadth_first_search/127.py
@@ -0,0 +1,24 @@
+import collections
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wl = set(wordList)
+        q = collections.deque([(beginWord, 1)])
+        seen = set([beginWord])
+
+        while q:
+            curr, steps = q.popleft()
+
+            if curr == endWord:
+                return steps
+
+            for i in range(len(curr)):
+                for c in "abcdefghijklmnopqrstuvwxyz":
+                    next_word = curr[:i] + c + curr[i + 1:]
+
+                    if next_word in wl and next_word not in seen:
+                        seen.add(next_word)
+                        q.append((next_word, steps + 1))
+
+        return 0
