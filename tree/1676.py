12fa855f521c471c83bea22775d8a25e6806da98
mujingw <mqw5130@gmail.com>
2021-03-06 08:04:51 -0500

    [LC-1676]

--- tree/1676.py
+++ tree/1676.py
@@ -1,26 +1,18 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', nodes: 'List[TreeNode]') -> 'TreeNode':
-        def lca(root, p, q):
+        def lca(root, nodes):
             if not root:
                 return None
 
-            if root == p or root == q:
+            if root in nodes:
                 return root
 
-            left = lca(root.left, p, q)
-            right = lca(root.right, p, q)
+            l = lca(root.left, nodes)
+            r = lca(root.right, nodes)
 
-            if left and right:
+            if l and r:
                 return root
+            else:
+                return l if l else r
 
-            return left if left else right
-
-        if len(nodes) == 1:
-            return nodes[0]
-
-        res = lca(root, nodes[0], nodes[1])
-
-        for node in nodes:
-            res = lca(root, res, node)
-
-        return res
+        return lca(root, set(nodes))
