2e044d01f9fcaad3d4c6a162e1b22a4f7c5c4bee
mujingw <mqw5130@gmail.com>
2021-03-05 20:57:06 -0500

    [LC-198]

--- dynamic_programming/198.py
+++ dynamic_programming/198.py
@@ -1,17 +1,18 @@
-from functools import lru_cache
 from typing import List
 
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        @lru_cache(None)
-        def helper(location):
-            if location < 0:
-                return 0
+        if not nums:
+            return 0
 
-            if location == 0:
-                return nums[0]
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
 
-            return max(nums[location] + helper(location - 2), helper(location - 1))
+        for i in range(1, len(nums)):
+            if i == 1:
+                dp[i] = max(dp[0], nums[1])
+            else:
+                dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
 
-        return helper(len(nums) - 1)
+        return max(dp)
