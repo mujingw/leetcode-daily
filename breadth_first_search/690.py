82e67258c822be168067875c7c8fb0a2e740c369
mujingw <mqw5130@gmail.com>
2021-03-07 12:11:22 -0500

    [LC-690]

---
+++ breadth_first_search/690.py
@@ -0,0 +1,28 @@
+import collections
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], eid: int) -> int:
+        imp = {}
+        g = defaultdict(set)
+        res = 0
+
+        for emp in employees:
+            imp[emp.id] = emp.importance
+
+            for ee in emp.subordinates:
+                g[emp.id].add(ee)
+
+        q = collections.deque([eid])
+        res = 0
+
+        while q:
+            curr = q.popleft()
+            res += imp[curr]
+
+            for sub in g[curr]:
+                q.append(sub)
+
+        return res
