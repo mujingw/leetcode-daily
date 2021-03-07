4afdaa04c02c61f57f8cb9beebeadd202cf0bb5e
mujingw <mqw5130@gmail.com>
2021-03-07 12:00:31 -0500

    [LC-99]

--- tree/99.py
+++ tree/99.py
@@ -1,6 +1,13 @@
 import collections
 
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def recoverTree(self, root: TreeNode) -> None:
         """
