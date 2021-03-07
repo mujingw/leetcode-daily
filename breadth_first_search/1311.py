d172de76638c3cfbbecf4ee867b089f1efc04758
mujingw <mqw5130@gmail.com>
2021-03-07 13:38:06 -0500

    [LC-1311]

---
+++ breadth_first_search/1311.py
@@ -0,0 +1,27 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def watchedVideosByFriends(self, watchedVideos: List[List[str]], friends: List[List[int]], my_id: int,
+                               level: int) -> List[str]:
+        q = collections.deque([(my_id, 0)])
+        level_k_movies = collections.defaultdict(int)
+        seen = set([my_id])
+
+        while q:
+            curr, lvl = q.popleft()
+
+            if lvl == level:
+                for video in watchedVideos[curr]:
+                    level_k_movies[video] += 1
+
+            if lvl > level:
+                break
+
+            for friend in friends[curr]:
+                if friend not in seen:
+                    seen.add(friend)
+                    q.append((friend, lvl + 1))
+
+        return [x[0] for x in sorted(list(level_k_movies.items()), key=lambda x: (x[1], x[0]))]
