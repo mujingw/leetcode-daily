4bd7d5c6a73fa4b449468493fe47badeb3cbd4d4
mujingw <mqw5130@gmail.com>
2021-03-10 14:51:08 -0500

    [LC-57]

---
+++ intervals/57.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def insert(self, intervals: List[List[int]], new_interval: List[int]) -> List[List[int]]:
+        all_intervals = intervals + [new_interval]
+        all_intervals.sort()
+        s, e = all_intervals[0][0], all_intervals[0][1]
+        res = []
+
+        for cs, ce in all_intervals[1:]:
+            if cs > e:
+                res.append([s, e])
+                s, e = cs, ce
+            else:
+                e = max(ce, e)
+
+        res.append([s, e])
+
+        return res
