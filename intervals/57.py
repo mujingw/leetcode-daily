1e47cb3724184a0ba6379153a152bc41af21ae8f
mujingw <mqw5130@gmail.com>
2021-03-10 15:01:18 -0500

    [LC-57]

--- intervals/57.py
+++ intervals/57.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Solution:
     def insert(self, intervals: List[List[int]], new_interval: List[int]) -> List[List[int]]:
-        all_intervals = intervals + [new_interval]
+        all_intervals = intervals + [new_interval, [float("inf"), float("inf")]]
         all_intervals.sort()
         s, e = all_intervals[0][0], all_intervals[0][1]
         res = []
@@ -14,7 +14,5 @@ class Solution:
                 s, e = cs, ce
             else:
                 e = max(ce, e)
-
-        res.append([s, e])
 
         return res
