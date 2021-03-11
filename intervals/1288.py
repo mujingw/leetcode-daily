e79e349e09f423ae44b04b70118373780cf81820
mujingw <mqw5130@gmail.com>
2021-03-11 18:52:42 -0500

    [LC-1288]

---
+++ intervals/1288.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        covered = 0
+        my_intervals = sorted(intervals + [[float("inf"), float("inf")]], key=lambda x: (x[0], -x[1]))
+        s, e = my_intervals[0][0], my_intervals[0][1]
+
+        for cs, ce in my_intervals[1:]:
+            if cs >= s and ce <= e:
+                covered += 1
+            else:
+                s, e = cs, ce
+
+        return len(intervals) - covered
