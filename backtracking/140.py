657f5f7298a4fdeffa74de4022d60cd38db3e86d
mujingw <mqw5130@gmail.com>
2021-03-16 18:56:11 -0400

    [LC-140]

---
+++ backtracking/140.py
@@ -0,0 +1,26 @@
+from functools import lru_cache
+from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        @lru_cache(None)
+        def helper(s):
+            res = []
+
+            if s in w_dict:
+                res.append(s)
+
+            for i in range(1, len(s)):
+                right = s[i:]
+
+                if right not in w_dict:
+                    continue
+
+                res.extend([left + " " + right for left in helper(s[0:i])])
+
+            return res
+
+        w_dict = set(wordDict)
+
+        return helper(s)
