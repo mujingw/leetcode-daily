736767acda6f43a8bfda2763ea795b8d63f3d9b1
mujingw <mqw5130@gmail.com>
2021-03-19 19:27:31 -0400

    [LC-45]

---
+++ breadth_first_search/45.py
@@ -0,0 +1,20 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        q = collections.deque([(0, 0)])
+        seen = set([0])
+        N = len(nums)
+
+        while q:
+            curr, dist = q.popleft()
+
+            if curr == N - 1:
+                return dist
+
+            for neig in range(curr - nums[curr], curr + nums[curr] + 1):
+                if 0 <= neig < N and neig not in seen:
+                    seen.add(neig)
+                    q.append((neig, dist + 1))
