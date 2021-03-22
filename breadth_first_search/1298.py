837dd6a39ca64952fde429154f6d7420ad718958
mujingw <mqw5130@gmail.com>
2021-03-22 04:41:08 -0400

    [LC-1298]

---
+++ breadth_first_search/1298.py
@@ -0,0 +1,37 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def maxCandies(self, status: List[int], candies: List[int], keys: List[List[int]], containedBoxes: List[List[int]],
+                   initialBoxes: List[int]) -> int:
+        q = collections.deque(initialBoxes)
+        seen = set(initialBoxes)
+        res = 0
+        on_hold = set([])
+        all_keys = set([])
+
+        while q:
+            curr = q.popleft()
+            res += candies[curr]
+            all_keys.update(set(keys[curr]))
+
+            potential_new_boxes = all_keys.intersection(on_hold)
+
+            for box in potential_new_boxes:
+                if box not in seen and status[box] == 0:
+                    seen.add(box)
+                    q.append(box)
+
+            for box in list(on_hold):
+                if box in seen:
+                    on_hold.remove(box)
+
+            for box in containedBoxes[curr]:
+                if (box in all_keys or status[box] == 1) and box not in seen:
+                    q.append(box)
+                    seen.add(box)
+                elif status[box] == 0 and box not in seen:
+                    on_hold.add(box)
+
+        return res
