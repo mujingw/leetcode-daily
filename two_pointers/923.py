0a568a0570257e1f3dec51ca93f802a984cfb90c
mujingw <mqw5130@gmail.com>
2021-03-23 03:31:54 -0400

    [LC-923]

---
+++ two_pointers/923.py
@@ -0,0 +1,52 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def threeSumMulti(self, arr: List[int], target: int) -> int:
+        def n_choose_2(n):
+            return n * (n - 1) // 2
+
+        def n_choose_3(n):
+            return n * (n - 1) * (n - 2) // 6
+
+        counter = collections.Counter(arr)
+        arr.sort()
+        MOD, N, res = 10 ** 9 + 7, len(arr), 0
+
+        for i in range(N - 2):
+            if i > 0 and arr[i] == arr[i - 1]:
+                continue
+
+            l, r = i + 1, N - 1
+
+            while l < r:
+                a, b, c = arr[i], arr[l], arr[r]
+                s = a + b + c
+
+                if s == target:
+                    if a == b == c:
+                        res += n_choose_3(counter[a])
+                    elif a == b:
+                        res += (n_choose_2(counter[a]) * counter[c])
+                    elif b == c:
+                        res += (n_choose_2(counter[b]) * counter[a])
+                    else:
+                        res += (counter[a] * counter[b] * counter[c])
+
+                    l += 1
+
+                    while l < r and arr[l] == arr[l - 1]:
+                        l += 1
+
+                    r -= 1
+
+                    while l < r and arr[r] == arr[r + 1]:
+                        r -= 1
+
+                elif s < target:
+                    l += 1
+                else:
+                    r -= 1
+
+        return res % MOD
