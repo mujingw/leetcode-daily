7642323c284e9d425315dc2e4417dfcf07c88049
mujingw <mqw5130@gmail.com>
2021-03-25 03:25:32 -0400

    [LC-417]

--- breadth_first_search/417.py
+++ breadth_first_search/417.py
@@ -4,39 +4,35 @@ from typing import List
 
 class Solution:
     def pacificAtlantic(self, matrix: List[List[int]]) -> List[List[int]]:
-        def bfs(x, y, R, C, DIR):
-            q = collections.deque([(x, y)])
-            seen = set((x, y))
-            res = [False, False]
+        def bfs(r0, c0):
+            q = collections.deque([(r0, c0)])
+            seen = set([(r0, c0)])
 
             while q:
                 cx, cy = q.popleft()
 
-                if cx == 0 or cy == 0:
-                    res[0] = True
-
-                if cx == R - 1 or cy == C - 1:
-                    res[1] = True
-
                 for dx, dy in DIR:
                     nx, ny = cx + dx, cy + dy
 
-                    if 0 <= nx < R and 0 <= ny < C and (nx, ny) not in seen and matrix[nx][ny] <= matrix[cx][cy]:
+                    if 0 <= nx < R and 0 <= ny < C and (nx, ny) not in seen and matrix[nx][ny] >= matrix[cx][cy]:
                         seen.add((nx, ny))
                         q.append((nx, ny))
 
-            return all(res)
+            return seen
 
-        if not matrix or not matrix[0]:
+        if not matrix:
             return []
 
+        good_pac, good_atl = set(), set()
         R, C = len(matrix), len(matrix[0])
         DIR = ((1, 0), (0, 1), (0, -1), (-1, 0))
-        res = []
 
-        for r in range(R):
-            for c in range(C):
-                if bfs(r, c, R, C, DIR):
-                    res.append([r, c])
+        for i in range(C):
+            good_pac |= bfs(0, i)
+            good_atl |= bfs(R - 1, i)
 
-        return res
+        for i in range(R):
+            good_pac |= bfs(i, 0)
+            good_atl |= bfs(i, C - 1)
+
+        return good_pac.intersection(good_atl)
