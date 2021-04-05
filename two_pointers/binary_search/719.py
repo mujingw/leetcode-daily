12f15322727dc54fdfa1233b24addfb4601c4a98
mujingw <mqw5130@gmail.com>
2021-04-05 05:31:47 -0400

    [LC-719]

--- two_pointers/binary_search/719.py
+++ two_pointers/binary_search/719.py
@@ -4,21 +4,17 @@ from typing import List
 class Solution:
     def smallestDistancePair(self, nums: List[int], k: int) -> int:
         def ok(max_dist_allowed):
-            res, l, r = 0, 0, 0
+            res, left = 0, 0
 
-            while r < N:
-                r_val = sn[r]
-                r += 1
+            for right, val in enumerate(sn):
+                while val - sn[left] > max_dist_allowed:
+                    left += 1
 
-                while r_val - sn[l] > max_dist_allowed:
-                    l += 1
-
-                res += (r - l - 1)
+                res += (right - left)
 
             return res >= k
 
         sn = sorted(nums)
-        N = len(nums)
         l, r = 0, sn[-1] - sn[0]
 
         while l < r:
