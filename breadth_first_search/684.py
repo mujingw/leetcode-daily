1d08209976dea0cc7ecb50d1ddc00f0a01609ab8
mujingw <mqw5130@gmail.com>
2021-04-20 23:07:20 -0400

    [LC-684]

---
+++ breadth_first_search/684.py
@@ -0,0 +1,34 @@
+from collections import deque, defaultdict
+from typing import List
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        def bfs(src, tgt):
+            q = deque([src])
+            seen = {src}
+
+            while q:
+                curr = q.popleft()
+
+                if curr == tgt:
+                    return True
+
+                for neig in g[curr]:
+                    if neig not in seen:
+                        seen.add(neig)
+                        q.append(neig)
+
+            return False
+
+        g = defaultdict(set)
+        res = None
+
+        for u, v in edges:
+            if not bfs(u, v):
+                g[u].add(v)
+                g[v].add(u)
+            else:
+                res = [u, v]
+
+        return res
