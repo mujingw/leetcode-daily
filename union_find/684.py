b68be8fb65063b2591fa5a8de62a6fbbcc5bb1de
mujingw <mqw5130@gmail.com>
2021-04-21 04:30:57 -0400

    [LC-684]

---
+++ union_find/684.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+class UF:
+    def __init__(self, size):
+        self.rank = [0] * size
+        self.parent = [i for i in range(size)]
+
+    def union(self, u, v):
+        ru, rv = self.find(u), self.find(v)
+
+        if ru != rv:
+            if self.rank[ru] < self.rank[rv]:
+                self.parent[rv] = ru
+            elif self.rank[ru] > self.rank[rv]:
+                self.parent[ru] = rv
+            else:
+                self.parent[rv] = ru
+                self.rank[ru] += 1
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+
+        return self.parent[x]
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        uf = UF(len(edges) + 1)
+
+        for u, v in edges:
+            root_u, root_v = uf.find(u), uf.find(v)
+
+            if root_u != root_v:
+                uf.union(u, v)
+            else:
+                return [u, v]
