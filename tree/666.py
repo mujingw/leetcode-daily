317acbb0d88d4711b9c0020b2ba98b2779c91c68
mujingw <mqw5130@gmail.com>
2021-05-03 20:02:26 -0400

    [LC-666]

--- tree/666.py
+++ tree/666.py
@@ -9,9 +9,10 @@ class Solution:
 
         for num in nums[::-1]:
             lvl, pos, val = num // 100, num // 10 % 10, num % 10
-            node_appearances[lvl, pos] = max(1, node_appearances[lvl + 1, pos * 2 - 1] + node_appearances[
-                lvl + 1, pos * 2])
-            sum_at_node[lvl, pos] = sum_at_node[lvl + 1, pos * 2 - 1] \
+            node_appearances[lvl, pos] \
+                = max(1, node_appearances[lvl + 1, pos * 2 - 1] + node_appearances[lvl + 1, pos * 2])
+            sum_at_node[lvl, pos] \
+                = sum_at_node[lvl + 1, pos * 2 - 1] \
                 + sum_at_node[lvl + 1, pos * 2] \
                 + node_appearances[lvl, pos] * val
 
