10c0f6af3b55390cec9a8e86af3738d8117e12c5
mujingw <mqw5130@gmail.com>
2021-05-05 18:24:49 -0400

    [LC-216]

--- backtracking/216.py
+++ backtracking/216.py
@@ -1,26 +1,24 @@
-class Solution(object):
-    def combinationSum3(self, k, n):
-        """
-        :type k: int
-        :type n: int
-        :rtype: List[List[int]]
-        """
+from typing import List
 
-        def dfs(res, curr, k, needed, start):
-            if needed < 0 or len(curr) > k:
-                return
 
-            if len(curr) == k and needed == 0:
-                res.append(curr[:])
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        def backtrack(res, curr, start, t):
+            if len(curr) == k:
+                if t == 0:
+                    res.append(curr[:])
 
                 return
 
-            for num in range(start, 10):
-                curr.append(num)
-                dfs(res, curr, k, needed - num, num + 1)
+            for i in range(start, 10):
+                if t - i < 0:
+                    continue
+
+                curr.append(i)
+                backtrack(res, curr, i + 1, t - i)
                 curr.pop()
 
         res = []
-        dfs(res, [], k, n, 1)
+        backtrack(res, [], 1, n)
 
         return res
