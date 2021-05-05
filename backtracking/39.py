6d54b36fb6c4dedbdd84cd25e5bc583a9c0f5457
mujingw <mqw5130@gmail.com>
2021-05-05 18:37:50 -0400

    [LC-39]

--- backtracking/39.py
+++ backtracking/39.py
@@ -1,29 +1,22 @@
-class Solution(object):
-    def combinationSum(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
+from typing import List
 
-        def dfs(res, curr, target, nums, start):
-            if target < 0:
-                return
 
-            if target == 0:
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def backtrack(res, curr, t, nums, start):
+            if t == 0:
                 res.append(curr[:])
-
                 return
 
             for i in range(start, len(nums)):
-                if nums[i] > target:
+                if t - nums[i] < 0:
                     break
 
                 curr.append(nums[i])
-                dfs(res, curr, target - nums[i], nums, i)
+                backtrack(res, curr, t - nums[i], nums, i)
                 curr.pop()
 
         res = []
-        dfs(res, [], target, sorted(candidates), 0)
+        backtrack(res, [], target, sorted(candidates), 0)
 
         return res
