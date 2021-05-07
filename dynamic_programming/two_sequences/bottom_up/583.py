6a46cb381057c98f814e6e03889f0e887e3e60c4
mujingw <mqw5130@gmail.com>
2021-05-07 14:19:27 -0400

    [LC-583]

--- dynamic_programming/two_sequences/bottom_up/583.py
+++ dynamic_programming/two_sequences/bottom_up/583.py
@@ -3,17 +3,16 @@ class Solution:
         N1, N2 = len(word1), len(word2)
         dp = [[0 for _ in range(N2 + 1)] for _ in range(N1 + 1)]
 
-        for row in range(N1 + 1):
-            dp[row][0] = row
-
-        for col in range(N2 + 1):
-            dp[0][col] = col
-
-        for i in range(1, N1 + 1):
-            for j in range(1, N2 + 1):
-                if word1[i - 1] == word2[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1]
+        for i in range(N1 + 1):
+            for j in range(N2 + 1):
+                if i == 0:
+                    dp[i][j] = j
+                elif j == 0:
+                    dp[i][j] = i
                 else:
-                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + 1
+                    if word1[i - 1] == word2[j - 1]:
+                        dp[i][j] = dp[i - 1][j - 1]
+                    else:
+                        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + 1
 
         return dp[N1][N2]
