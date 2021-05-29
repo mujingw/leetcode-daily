410cd69bf5f4bfe5fc3c4d8f6677e36df6fb2d4c
mujingw <mqw5130@gmail.com>
2021-05-29 00:57:07 -0400

    [LC-1590]

---
+++ two_pointers/pre_sum/1590.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        presum = {0: -1}
+        goal = sum(nums) % p
+
+        if goal == 0:
+            return 0
+
+        N = len(nums)
+        running, res = 0, N
+
+        for i, v in enumerate(nums):
+            running = (running + v) % p
+            target = (running - goal) % p
+            presum[running] = i
+
+            if target in presum:
+                res = min(res, i - presum[target])
+
+        return res if res < N else -1
