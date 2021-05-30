4483b0f544f387f2908f0e717acbcbb1cd042e0e
mujingw <mqw5130@gmail.com>
2021-05-29 21:46:46 -0400

    [LC-395]

--- divide_and_conquer/395.py
+++ divide_and_conquer/395.py
@@ -3,12 +3,12 @@ from collections import Counter
 
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
-        bad_chars = [ch for ch, count in Counter(s).items() if count < k]
+        invalid_chars = [ch for ch, count in Counter(s).items() if count < k]
 
-        if bad_chars:
+        if len(invalid_chars) > 0:
             best = 0
 
-            for sub_string in s.split(bad_chars[0]):
+            for sub_string in s.split(invalid_chars[0]):
                 if len(sub_string) >= k:
                     best = max(best, self.longestSubstring(sub_string, k))
 
