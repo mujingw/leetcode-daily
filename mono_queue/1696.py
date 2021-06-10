b356666cbdcec5106a8ceb153b7c09cf6f90fc47
mujingw <mqw5130@gmail.com>
2021-06-09 22:12:35 -0400

    [LC-1696]

--- mono_queue/1696.py
+++ mono_queue/1696.py
@@ -1,21 +1,37 @@
 from collections import deque
 
 
+class MonoQueue:
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x):
+        while self.q and self.q[-1] < x:
+            self.q.pop()
+
+        self.q.append(x)
+
+    def pop(self, x):
+        if self.q and x == self.q[0]:
+            self.q.popleft()
+
+    def mx(self):
+        return self.q[0]
+
+
 class Solution(object):
     def maxResult(self, nums, k):
-        dp = [float("-inf")] * len(nums)
+        mono_q = MonoQueue()
+        N = len(nums)
+        dp = [0] * N
         dp[0] = nums[0]
-        mono_q = deque([0])
+        mono_q.push(dp[0])
 
-        for i, num in enumerate(nums[1:], 1):
-            while mono_q and mono_q[0] < i - k:
-                mono_q.popleft()
+        for i, v in enumerate(nums[1:], 1):
+            if i - k - 1 >= 0:
+                mono_q.pop(dp[i - k - 1])
 
-            dp[i] = dp[mono_q[0]] + num
-
-            while mono_q and dp[mono_q[-1]] < dp[i]:
-                mono_q.pop()
-
-            mono_q.append(i)
+            dp[i] = mono_q.mx() + v
+            mono_q.push(dp[i])
 
         return dp[-1]
