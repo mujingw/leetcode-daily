751109c437151aac31afa4e0a1cf2f47571b4eae
mujingw <mqw5130@gmail.com>
2021-06-13 13:28:19 -0400

    [LC-490]

--- breadth_first_search/490.py
+++ breadth_first_search/490.py
@@ -1,32 +1,29 @@
-import collections
+from collections import deque
 from typing import List
 
 
 class Solution:
-    def hasPath(self, maze: List[List[int]], start: List[int], dest: List[int]) -> bool:
-        q = collections.deque([(start[0], start[1])])
-        seen = set([(start[0], start[1])])
+    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
+        q = deque([(start[0], start[1])])
+        seen = {(start[0], start[1])}
         DIR = ((0, 1), (0, -1), (-1, 0), (1, 0))
         R, C = len(maze), len(maze[0])
 
         while q:
             cx, cy = q.popleft()
 
-            if cx == dest[0] and cy == dest[1]:
+            if cx == destination[0] and cy == destination[1]:
                 return True
 
             for dx, dy in DIR:
                 nx, ny = cx, cy
 
-                while 0 <= nx < R and 0 <= ny < C and maze[nx][ny] != 1:
+                while 0 <= nx + dx < R and 0 <= ny + dy < C and maze[nx + dx][ny + dy] != 1:
                     nx += dx
                     ny += dy
 
-                nx -= dx
-                ny -= dy
-
-                if 0 <= nx < R and 0 <= ny < C and (nx, ny) not in seen and maze[nx][ny] == 0:
-                    q.append((nx, ny))
+                if (nx, ny) not in seen:
                     seen.add((nx, ny))
+                    q.append((nx, ny))
 
         return False
