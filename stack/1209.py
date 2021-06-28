7df308e7a31354d09690087d2c25763867a813ef
mujingw <mqw5130@gmail.com>
2021-06-29 01:05:58 +0800

    [LC-1209]

--- stack/1209.py
+++ stack/1209.py
@@ -3,21 +3,12 @@ class Solution:
         stack = []
 
         for ch in s:
-            if not stack:
-                stack.append((ch, 1))
+            if not stack or stack[-1][0] != ch:
+                stack.append([ch, 1])
             else:
-                prev_ch, count = stack[-1]
-
-                if prev_ch == ch:
-                    stack.append((ch, count + 1))
-                else:
-                    stack.append((ch, 1))
-
-            if stack and stack[-1][1] == k:
-                p = k
-
-                while p > 0:
+                if stack[-1][1] == k - 1:
                     stack.pop()
-                    p -= 1
+                else:
+                    stack[-1][1] += 1
 
-        return "".join([ch for ch, count in stack])
+        return "".join([ch * count for ch, count in stack])
