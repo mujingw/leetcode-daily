1ba3dfff8e64480fc2296d3594b44f00c2091b19
mujingw <mqw5130@gmail.com>
2021-07-01 18:24:53 +0800

    [LC-89]

--- bit_manipulation/89.py
+++ bit_manipulation/89.py
@@ -3,16 +3,14 @@ from typing import List
 
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        if n == 1:
-            return [0, 1]
-
         res = [0, 1]
 
         for i in range(1, n):
+            mask = 1 << i
             curr = []
 
             for code in res[::-1]:
-                curr.append((1 << i) ^ code)
+                curr.append(mask ^ code)
 
             res.extend(curr)
 
