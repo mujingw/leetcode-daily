f21aeebc140c6d860fe6becaecf95a702dd42cde
mujingw <mqw5130@gmail.com>
2021-08-19 13:52:30 +0800

    [LC-268]

---
+++ bit_manipulation/268.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = len(nums)
+
+        for i, v in enumerate(nums):
+            res ^= (i ^ v)
+
+        return res
