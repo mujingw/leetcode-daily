e348758809e3866f30d4c04cfbdd67e072819c8e
mujingw <mqw5130@gmail.com>
2021-08-21 19:42:43 +0800

    [LC-137]

---
+++ bit_manipulation/137.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        N = len(nums)
+        ones, twos = 0, 0
+
+        for i in range(N):
+            ones = (ones ^ nums[i]) & ~twos;
+            twos = (twos ^ nums[i]) & ~ones;
+
+        return ones
