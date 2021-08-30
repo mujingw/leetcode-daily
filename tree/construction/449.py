591b0262db0ea55a2325abf29a91f1e9b1b66e39
mujingw <mqw5130@gmail.com>
2021-08-30 21:15:03 +0800

    [LC-449]

--- tree/construction/449.py
+++ tree/construction/449.py
@@ -12,34 +12,44 @@ class TreeNode:
 class Codec:
 
     def __init__(self):
-        self.NULL_NODE = 'X'
         self.DELIMITER = ','
 
     def serialize(self, root: TreeNode) -> str:
         """Encodes a tree to a single string.
         """
-        if not root:
-            return self.NULL_NODE
 
-        return self.DELIMITER.join([str(root.val), self.serialize(root.left), self.serialize(root.right)])
+        def helper(res, root):
+            if not root:
+                return
+
+            res.append(str(root.val))
+            helper(res, root.left)
+            helper(res, root.right)
+
+        res = []
+        helper(res, root)
+
+        return self.DELIMITER.join(res)
 
     def deserialize(self, data: str) -> TreeNode:
         """Decodes your encoded data to tree.
         """
 
-        def helper(q):
-            curr = q.popleft()
-
-            if curr == self.NULL_NODE:
+        def helper(q, lower, upper):
+            if q and q[0] == '':
                 return None
 
+            if not (q and lower <= int(q[0]) <= upper):
+                return None
+
+            curr = int(q.popleft())
             root = TreeNode(curr)
-            root.left = helper(q)
-            root.right = helper(q)
+            root.left = helper(q, lower, curr)
+            root.right = helper(q, curr, upper)
 
             return root
 
-        return helper(deque(data.split(self.DELIMITER)))
+        return helper(deque(data.split(self.DELIMITER)), float('-inf'), float('inf'))
 
 # Your Codec object will be instantiated and called as such:
 # Your Codec object will be instantiated and called as such:
