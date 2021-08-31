57556f650e69294752c6d2f9284cbb44cd41214c
mujingw <mqw5130@gmail.com>
2021-09-01 03:10:13 +0800

    [LC-261]

--- union_find/261.py
+++ union_find/261.py
@@ -3,11 +3,14 @@ from typing import List
 
 class UF:
     def __init__(self, size):
-        self.rank = [0] * size
-        self.parent = [i for i in range(size)]
+        self.rank = {i: 1 for i in range(size)}
+        self.parent = {i: i for i in range(size)}
 
     def union(self, u, v):
         ru, rv = self.find(u), self.find(v)
+
+        if ru == rv:
+            return False
 
         if self.rank[ru] < self.rank[rv]:
             self.parent[ru] = rv
@@ -17,26 +20,20 @@ class UF:
             self.parent[rv] = ru
             self.rank[ru] += 1
 
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
+        return True
 
-        return self.parent[x]
+    def find(self, p):
+        if self.parent[p] != p:
+            self.parent[p] = self.find(self.parent[p])
+
+        return self.parent[p]
 
 
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if not len(edges) == n - 1:
+        if len(edges) != n - 1:
             return False
 
         uf = UF(n)
 
-        for u, v in edges:
-            ru, rv = uf.find(u), uf.find(v)
-
-            if ru != rv:
-                uf.union(u, v)
-            else:
-                return False
-
-        return True
+        return all(uf.union(u, v) for u, v in edges)
