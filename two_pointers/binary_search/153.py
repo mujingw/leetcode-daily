21e2f43224714c03acc9ebb4e24e96677c7a3418
mujingw <mqw5130@gmail.com>
2021-08-31 15:45:28 +0800

    [LC-153]

---
+++ two_pointers/binary_search/153.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        N = len(nums)
+        l, r = 0, N - 1
+
+        while l < r:
+            mid = (l + r) // 2
+
+            if nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+
+        return nums[l]
