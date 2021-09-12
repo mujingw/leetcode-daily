92c9c426d62b0251c3b254d35a4645b5ea7d0930
mujingw <mqw5130@gmail.com>
2021-09-12 20:15:43 +0800

    [LC-1740]

--- breadth_first_search/1740.py
+++ breadth_first_search/1740.py
@@ -1,11 +1,18 @@
-import collections
+from collections import defaultdict, deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 class Solution:
     def findDistance(self, root: TreeNode, p: int, q: int) -> int:
         def build_graph_find_src(root, p):
-            q = collections.deque([root])
-            g = collections.defaultdict(set)
+            q = deque([root])
+            g = defaultdict(set)
             src = None
 
             while q:
@@ -27,7 +34,7 @@ class Solution:
             return g, src
 
         def bfs(g, src, target):
-            q = collections.deque([(src, 0)])
+            q = deque([(src, 0)])
             seen = set([src])
 
             while q:
