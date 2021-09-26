fbea4415c1396f9792587225d3869d40c348dc8c
mujingw <mqw5130@gmail.com>
2021-09-26 14:17:03 +0800

    [LC-695]

--- depth_first_search/grids/695.py
+++ depth_first_search/grids/695.py
@@ -1,17 +1,17 @@
+from itertools import product
 from typing import List
 
 
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        R, C = len(grid), len(grid[0])
-        DIR = ((0, 1), (0, -1), (-1, 0), (1, 0))
-        visited = set()
-
         def dfs(x, y):
             if not (0 <= x < R and 0 <= y < C):
                 return 0
 
-            if (x, y) in visited or grid[x][y] == 0:
+            if grid[x][y] == 0:
+                return 0
+
+            if (x, y) in visited:
                 return 0
 
             res = 1
@@ -22,11 +22,12 @@ class Solution:
 
             return res
 
+        R, C = len(grid), len(grid[0])
+        DIR = ((0, 1), (0, -1), (-1, 0), (1, 0))
+        visited = set()
         res = 0
 
-        for r in range(R):
-            for c in range(C):
-                if grid[r][c] == 1:
-                    res = max(res, dfs(r, c))
+        for r, c in product(range(R), range(C)):
+            res = max(res, dfs(r, c))
 
         return res
