cd873d8cd757468354fb3d08c5e9c83f1912ab91
mujingw <mqw5130@gmail.com>
2021-09-27 14:27:37 +0800

    [LC-17]

--- backtracking/17.py
+++ backtracking/17.py
@@ -3,21 +3,24 @@ from typing import List
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        def dfs(res, curr, pos, digits, phone):
+        def backtrack(res, curr, pos):
             if len(curr) == len(digits):
-                res.append("".join(curr))
+                res.append(''.join(curr))
+
                 return
 
-            for ch in phone[digits[pos]]:
-                dfs(res, curr + [ch], pos + 1, digits, phone)
+            for ch in PHONE[digits[pos]]:
+                curr.append(ch)
+                backtrack(res, curr, pos + 1)
+                curr.pop()
 
         if not digits:
-            return []
+            return ""
 
-        phone = {"2": "abc", "3": "def",
-                 "4": "ghi", "5": "jkl", "6": "mno",
-                 "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        PHONE = {'2': 'abc', '3': 'def', \
+                 '4': 'ghi', '5': 'jkl', '6': 'mno', \
+                 '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
         res = []
-        dfs(res, [], 0, digits, phone)
+        backtrack(res, [], 0)
 
         return res
