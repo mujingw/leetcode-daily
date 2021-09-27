40a318e8d2bc37315d24f039b561a981bca79238
mujingw <mqw5130@gmail.com>
2021-09-27 14:29:06 +0800

    [LC-17]

--- backtracking/17.py
+++ backtracking/17.py
@@ -15,10 +15,10 @@ class Solution:
                 curr.pop()
 
         if not digits:
-            return ""
+            return ''
 
-        PHONE = {'2': 'abc', '3': 'def', \
-                 '4': 'ghi', '5': 'jkl', '6': 'mno', \
+        PHONE = {'2': 'abc', '3': 'def',
+                 '4': 'ghi', '5': 'jkl', '6': 'mno',
                  '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
         res = []
         backtrack(res, [], 0)
