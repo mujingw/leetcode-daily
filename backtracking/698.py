039957d71a5f8bc86ffb23e3bca931e91f004124
mujingw <mqw5130@gmail.com>
2021-09-30 18:59:43 +0800

    [LC-698]

--- backtracking/698.py
+++ backtracking/698.py
@@ -3,38 +3,35 @@ from typing import List
 
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
-        N = len(nums)
-        used = [False] * N
-        total = sum(nums)
+        self.total = sum(nums)
+        self.k = k
+
+        if self.total % self.k != 0:
+            return False
+
         nums.sort(reverse=True)
 
-        if total % k != 0:
-            return False
+        return self.dfs(nums, self.total // self.k, 0, 0, [False] * len(nums))
 
-        def dfs(target, pos, count):
-            if count == k:
+    def dfs(self, nums, target, pos, count, used):
+        if count == self.k:
+            return True
+
+        if target == 0:
+            return self.dfs(nums, self.total // self.k, 0, count + 1, used)
+
+        last_failed_num = -1
+
+        for i in range(pos, len(nums)):
+            if used[i] or nums[i] > target or nums[i] == last_failed_num:
+                continue
+
+            used[i] = True
+
+            if self.dfs(nums, target - nums[i], i + 1, count, used):
                 return True
 
-            if target < 0:
-                return False
+            used[i] = False
+            last_failed_num = nums[i]
 
-            if target == 0:
-                return dfs(total // k, 0, count + 1)
-
-            last_failure_num = -1
-
-            for i in range(pos, N):
-                if used[i] or nums[i] == last_failure_num or nums[i] > target:
-                    continue
-
-                used[i] = True
-
-                if dfs(target - nums[i], i + 1, count):
-                    return True
-
-                used[i] = False
-                last_failure_num = nums[i]
-
-            return False
-
-        return dfs(total // k, 0, 0)
+        return False
