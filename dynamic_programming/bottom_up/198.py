2284fd45d1648b3266acbc7d0797dafa704a1e16
mujingw <mqw5130@gmail.com>
2021-10-05 16:10:21 +0800

    [LC-198]

--- dynamic_programming/bottom_up/198.py
+++ dynamic_programming/bottom_up/198.py
@@ -3,16 +3,15 @@ from typing import List
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
+        N = len(nums)
 
-        dp = [0] * len(nums)
-        dp[0] = nums[0]
+        if N < 2:
+            return nums[0]
 
-        for i in range(1, len(nums)):
-            if i == 1:
-                dp[i] = max(dp[0], nums[1])
-            else:
-                dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+        dp = [0] * N
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
 
-        return max(dp)
+        for i in range(2, N):
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
+
+        return dp[-1]
