75fc81724e80a95b48e78455ceba3aadffece7a7
mujingw <mqw5130@gmail.com>
2021-10-20 04:18:29 +0800

    [LC-450]

---
+++ tree/modification/450.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not root:
+            return None
+
+        if root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.left and not root.right:
+                root = None
+            elif root.right:
+                root.val = self.successor(root)
+                root.right = self.deleteNode(root.right, root.val)
+            else:
+                root.val = self.predecessor(root)
+                root.left = self.deleteNode(root.left, root.val)
+
+        return root
+
+    def predecessor(self, node):
+        curr = node.left
+
+        while curr.right:
+            curr = curr.right
+
+        return curr.val
+
+    def successor(self, node):
+        curr = node.right
+
+        while curr.left:
+            curr = curr.left
+
+        return curr.val
