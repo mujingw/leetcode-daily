2595a6b8f074ef3e9477ef2fa7a18122d8052200
mujingw <mqw5130@gmail.com>
2021-10-26 05:59:34 +0800

    [LC-1312]

--- dynamic_programming/two_sequences/bottom_up/1312.py
+++ dynamic_programming/two_sequences/bottom_up/1312.py
@@ -3,14 +3,11 @@ class Solution:
         return len(s) - self.lcs(s, s[::-1])
 
     def lcs(self, s1: str, s2: str) -> int:
-        N = len(s2)
-        dp = [[0] * (N + 1) for _ in range(N + 1)]
+        N = len(s1)
+        dp = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
 
         for i, a in enumerate(s1):
             for j, b in enumerate(s2):
-                if a == b:
-                    dp[i + 1][j + 1] = dp[i][j] + 1
-                else:
-                    dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
+                dp[i + 1][j + 1] = dp[i][j] + 1 if a == b else max(dp[i + 1][j], dp[i][j + 1])
 
         return dp[-1][-1]
