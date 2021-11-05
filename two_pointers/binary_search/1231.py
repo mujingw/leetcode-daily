b9594ac4fa5c9372e94a72ba020901f944d7615d
mujingw <mqw5130@gmail.com>
2021-11-05 15:49:10 +0800

    [LC-1231]

--- two_pointers/binary_search/1231.py
+++ two_pointers/binary_search/1231.py
@@ -2,30 +2,27 @@ from typing import List
 
 
 class Solution:
-    def maximizeSweetness(self, sweetness: List[int], K: int) -> int:
-        def ok(at_least):
-            curr_total, cuts = 0, 0
-
-            for s in sweetness:
-                if curr_total >= at_least and cuts < K:
-                    cuts += 1
-                    curr_total = s
-                else:
-                    curr_total += s
-
-            return curr_total >= at_least and cuts == K
-
+    def maximizeSweetness(self, sweetness: List[int], k: int) -> int:
         l, r = min(sweetness), 10 ** 9 + 1
 
         while l < r:
             mid = (l + r) // 2
 
-            if ok(mid):
+            if self.ok(mid, sweetness, k):
                 l = mid + 1
             else:
                 r = mid
 
-        if ok(l):
-            return l
-        else:
-            return l - 1
+        return l if self.ok(l, sweetness, k) else l - 1
+
+    def ok(self, at_least, sweetness, K):
+        curr_total, cuts = 0, 0
+
+        for s in sweetness:
+            if curr_total >= at_least and cuts < K:
+                cuts += 1
+                curr_total = s
+            else:
+                curr_total += s
+
+        return curr_total >= at_least and cuts == K
