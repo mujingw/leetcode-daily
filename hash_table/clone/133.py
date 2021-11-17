96a742a37e2b0a006281873a808f36988202260a
mujingw <mqw5130@gmail.com>
2021-11-17 15:08:47 +0800

    [LC-133]

---
+++ hash_table/clone/133.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return None
+
+        d = {node: Node(node.val)}
+        q = deque([node])
+
+        while q:
+            curr = q.popleft()
+
+            for neig in curr.neighbors:
+                if neig not in d:
+                    d[neig] = Node(neig.val)
+                    q.append(neig)
+
+                d[curr].neighbors.append(d[neig])
+
+        return d[node]
