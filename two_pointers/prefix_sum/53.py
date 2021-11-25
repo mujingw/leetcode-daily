49929420b7dad78416c9d4bff5e4fb114e382e7a
mujingw <mqw5130@gmail.com>
2021-11-25 17:30:12 +0800

    [LC-53]

--- two_pointers/prefix_sum/53.py
+++ two_pointers/prefix_sum/53.py
@@ -3,11 +3,11 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        min_left_sum, total, res = 0, 0, float("-inf")
+        min_left, total, res = 0, 0, float('-inf')
 
-        for num in nums:
-            total += num
-            res = max(res, total - min_left_sum)
-            min_left_sum = min(min_left_sum, total)
+        for x in nums:
+            total += x
+            res = max(res, total - min_left)
+            min_left = min(min_left, total)
 
         return res
