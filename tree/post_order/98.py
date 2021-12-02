a46b6b1a602a4fe9e9b5a5202095701809a94bcd
mujingw <mqw5130@gmail.com>
2021-12-03 01:57:32 +0800

    [LC-98]

--- tree/post_order/98.py
+++ tree/post_order/98.py
@@ -11,16 +11,16 @@ class TreeNode:
 
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        return self.dfs(root)[2]
+        return self.postorder(root)[2]
 
-    def dfs(self, node):
+    def postorder(self, node):
         if not node:
             return float('inf'), float('-inf'), True
 
-        lmin, lmax, lok = self.dfs(node.left)
-        rmin, rmax, rok = self.dfs(node.right)
+        lmin, lmax, lok = self.postorder(node.left)
+        rmin, rmax, rok = self.postorder(node.right)
 
         if lmax < node.val < rmin:
-            return min(lmin, node.val), max(rmax, node.val), lok and rok
+            return min(lmin, node.val), max(node.val, rmax), lok and rok
         else:
             return float('-inf'), float('inf'), False
