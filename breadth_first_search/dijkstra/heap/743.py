936a540bcc05d96c408b00b10a27ea900d3dca32
mujingw <mqw5130@gmail.com>
2021-12-09 05:26:28 +0800

    [LC-743]

--- breadth_first_search/dijkstra/heap/743.py
+++ breadth_first_search/dijkstra/heap/743.py
@@ -4,24 +4,27 @@ from typing import List
 
 
 class Solution:
-
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         h = []
         heappush(h, (0, k))
-        seen = {k: 0}
-        g = defaultdict(set)
-
-        for u, v, w in times:
-            g[u].add((v, w))
+        seen = defaultdict(lambda: float('inf'))
+        seen[k] = 0
+        g = self.build_graph(times)
 
         while h:
             cost, curr = heappop(h)
 
             for nx, weight in g[curr]:
-                if nx in seen and cost + weight >= seen[nx]:
-                    continue
-
-                seen[nx] = cost + weight
-                heappush(h, (cost + weight, nx))
+                if cost + weight < seen[nx]:
+                    seen[nx] = cost + weight
+                    heappush(h, (cost + weight, nx))
 
         return max(seen.values()) if len(seen) == n else -1
+
+    def build_graph(self, times):
+        g = defaultdict(set)
+
+        for u, v, w in times:
+            g[u].add((v, w))
+
+        return g
