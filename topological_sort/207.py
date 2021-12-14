606a9ec1e5bb59cab99599fcc0216a3dc2bfd322
mujingw <mqw5130@gmail.com>
2021-12-15 02:25:38 +0800

    [LC-207]

--- topological_sort/207.py
+++ topological_sort/207.py
@@ -1,25 +1,32 @@
 from collections import defaultdict
-from functools import lru_cache
 from typing import List
 
 
 class Solution:
     def canFinish(self, N: int, pre: List[List[int]]) -> bool:
+        self.visited, self.visiting = set(), set()
+        self.g = self.build_graph(pre)
+
+        return all(self.dfs(course) for course in range(N))
+
+    def build_graph(self, pre):
         g = defaultdict(set)
-        visiting = set()
-
-        @lru_cache(None)
-        def dfs(node):
-            if node in visiting:
-                return False
-
-            visiting.add(node)
-            ok = all(dfs(neig) for neig in g[node])
-            visiting.remove(node)
-
-            return ok
 
         for course, prereq in pre:
             g[prereq].add(course)
 
-        return all(dfs(course) for course in range(N))
+        return g
+
+    def dfs(self, node):
+        if node in self.visited:
+            return True
+
+        if node in self.visiting:
+            return False
+
+        self.visiting.add(node)
+        ok = all(self.dfs(ng) for ng in self.g[node])
+        self.visiting.remove(node)
+        self.visited.add(node)
+
+        return ok
