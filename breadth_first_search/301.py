031eccf7730b7e342e6d83de1ff19b31cebe71c7
mujingw <mqw5130@gmail.com>
2022-01-23 14:23:20 -0800

    [LC-301]

--- breadth_first_search/301.py
+++ breadth_first_search/301.py
@@ -4,40 +4,40 @@ from typing import List
 
 class Solution:
     def removeInvalidParentheses(self, s: str) -> List[str]:
-        def is_valid(s):
-            count = 0
-
-            for ch in s:
-                if ch == '(':
-                    count += 1
-                elif ch == ')':
-                    count -= 1
-
-                if count < 0:
-                    return False
-
-            return count == 0
-
         q = deque([s])
         seen = {s}
 
         while q:
             size = len(q)
-            valid_s = []
+            valid_strings = []
 
             for _ in range(size):
                 curr = q.popleft()
 
-                if is_valid(curr):
-                    valid_s.append(curr)
+                if self.is_valid(curr):
+                    valid_strings.append(curr)
                     continue
 
                 for i in range(len(curr)):
-                    next_s = curr[:i] + curr[i + 1:]
+                    next_string = curr[:i] + curr[i + 1:]
 
-                    if next_s not in seen:
-                        q.append(next_s)
-                        seen.add(next_s)
+                    if next_string not in seen:
+                        q.append(next_string)
+                        seen.add(next_string)
 
-            if valid_s:
-                return valid_s
+            if valid_strings:
+                return valid_strings
+
+    def is_valid(self, s):
+        count = 0
+
+        for ch in s:
+            if ch == '(':
+                count += 1
+            elif ch == ')':
+                count -= 1
+
+            if count < 0:
+                return False
+
+        return count == 0
