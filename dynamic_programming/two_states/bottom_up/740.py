bb3932298e6be3c9f0c38be0799efdd962a4ebd1
mujingw <mqw5130@gmail.com>
2022-05-13 01:45:10 -0700

    [LC-740]

--- dynamic_programming/two_states/bottom_up/740.py
+++ dynamic_programming/two_states/bottom_up/740.py
@@ -5,16 +5,27 @@ from typing import List
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         c = Counter(nums)
+        houses = []
 
-        if len(c) == 1:
+        for i in range(min(c.keys()), max(c.keys()) + 1):
+            houses.append(i * c[i])
+
+        return self.rob(houses)
+
+    def rob(self, nums: List[int]) -> int:
+        N = len(nums)
+
+        if N == 1:
             return nums[0]
 
-        lower, upper = min(c.keys()), max(c.keys())
-        dp = [0] * (upper + 1)
-        dp[lower] = c[lower]
-        dp[lower + 1] = max(c[lower], c[lower + 1])
+        if N == 2:
+            return max(nums)
 
-        for i in range(lower, upper + 1):
-            dp[i] = max(dp[i - 1], dp[i - 2] + i * c[i])
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
 
         return dp[-1]
