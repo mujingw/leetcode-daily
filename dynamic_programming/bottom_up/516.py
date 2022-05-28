e9d9d5f22df3bae1d8c08bd4ba45eab80a99ade4
mujingw <mqw5130@gmail.com>
2022-05-28 02:38:09 -0700

    [LC-516]

--- dynamic_programming/bottom_up/516.py
+++ dynamic_programming/bottom_up/516.py
@@ -4,16 +4,16 @@ class Solution:
             return len(s)
 
         N = len(s)
-        dp = [[0 for j in range(N)] for i in range(N)]
+        dp = [[0 for _ in range(N)] for _ in range(N)]
 
         for i in range(N):
             dp[i][i] = 1
 
-        for i in range(N, -1, -1):
+        for i in range(N - 1, -1, -1):
             for j in range(i + 1, N):
                 if s[i] == s[j]:
                     dp[i][j] = dp[i + 1][j - 1] + 2
                 else:
-                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+                    dp[i][j] = max(dp[i][j - 1], dp[i + 1][j])
 
-        return dp[0][N - 1]
+        return dp[0][-1]
