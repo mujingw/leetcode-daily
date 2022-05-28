2b3c4fdefa767e9ada31e14df6253a83143a54fb
mujingw <mqw5130@gmail.com>
2022-05-27 23:18:43 -0700

    [LC-5]

--- dynamic_programming/bottom_up/5.py
+++ dynamic_programming/bottom_up/5.py
@@ -5,20 +5,22 @@ class Solution:
 
         N = len(s)
         dp = [[False for _ in range(N)] for _ in range(N)]
-        longest, start = 1, 0
+        res = [0, 0]
 
         for i in range(N):
             dp[i][i] = True
 
-        for i in range(N - 1, -1, -1):
-            for j in range(i + 1, N):
-                if s[i] == s[j] and (i + 1 == j or dp[i + 1][j - 1]):
-                    dp[i][j] = True
+        for i in range(N - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                res = [i, i + 1]
 
-                curr_len = j - i + 1
+        for r in range(N - 1, -1, -1):
+            for c in range(r + 2, N):
+                if s[r] == s[c] and dp[r + 1][c - 1]:
+                    dp[r][c] = True
 
-                if curr_len > longest and dp[i][j]:
-                    longest = curr_len
-                    start = i
+                    if c - r + 1 > res[1] - res[0]:
+                        res = [r, c]
 
-        return s[start:start + longest]
+        return s[res[0]:res[1] + 1]
