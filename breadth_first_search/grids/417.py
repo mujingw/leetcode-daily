b297ca64460f11330b0b351bc77eb10bf1b85925
mujingw <mqw5130@gmail.com>
2022-09-06 16:27:27 -0700

    [LC-417]

--- breadth_first_search/grids/417.py
+++ breadth_first_search/grids/417.py
@@ -3,36 +3,39 @@ from typing import List
 
 
 class Solution:
-    def pacificAtlantic(self, matrix: List[List[int]]) -> List[List[int]]:
-        def bfs(r0, c0):
-            q = deque([(r0, c0)])
-            seen = set([(r0, c0)])
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        if not heights:
+            return []
 
-            while q:
-                cx, cy = q.popleft()
+        R, C = len(heights), len(heights[0])
+        q_pac, q_atl = deque(), deque()
 
-                for dx, dy in DIR:
-                    nx, ny = cx + dx, cy + dy
+        for c in range(C):
+            q_pac.append((0, c))
+            q_atl.append((R - 1, c))
 
-                    if 0 <= nx < R and 0 <= ny < C and (nx, ny) not in seen and matrix[nx][ny] >= matrix[cx][cy]:
+        for r in range(R):
+            q_pac.append((r, 0))
+            q_atl.append((r, C - 1))
+
+        good_pac = self.bfs(q_pac, R, C, heights)
+        good_atl = self.bfs(q_atl, R, C, heights)
+
+        return list(good_pac.intersection(good_atl))
+
+    def bfs(self, q, R, C, heights):
+        DIR = ((1, 0), (0, 1), (0, -1), (-1, 0))
+        seen = set(list(q))
+
+        while q:
+            cx, cy = q.popleft()
+
+            for dx, dy in DIR:
+                nx, ny = cx + dx, cy + dy
+
+                if 0 <= nx < R and 0 <= ny < C and heights[nx][ny] >= heights[cx][cy]:
+                    if (nx, ny) not in seen:
                         seen.add((nx, ny))
                         q.append((nx, ny))
 
-            return seen
-
-        if not matrix:
-            return []
-
-        good_pac, good_atl = set(), set()
-        R, C = len(matrix), len(matrix[0])
-        DIR = ((1, 0), (0, 1), (0, -1), (-1, 0))
-
-        for i in range(C):
-            good_pac |= bfs(0, i)
-            good_atl |= bfs(R - 1, i)
-
-        for i in range(R):
-            good_pac |= bfs(i, 0)
-            good_atl |= bfs(i, C - 1)
-
-        return good_pac.intersection(good_atl)
+        return seen
