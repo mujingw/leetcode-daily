6ba8b13a27fe5baa6ea35776dd71456971605809
mujingw <mqw5130@gmail.com>
2022-09-12 00:43:58 -0700

    [LC-416]

--- dynamic_programming/top_down/416.py
+++ dynamic_programming/top_down/416.py
@@ -4,20 +4,24 @@ from typing import List
 
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        @lru_cache(None)
-        def helper(pos, t):
-            if pos == len(nums):
-                return True if t == 0 else False
-
-            if t == 0:
-                return True
-
-            return helper(pos + 1, t - nums[pos]) or helper(pos + 1, t)
-
+        self.nums = sorted(nums)
         total = sum(nums)
-        nums.sort(reverse=True)
 
         if total % 2 != 0:
             return False
 
-        return helper(0, total // 2)
+        return self.dfs(total // 2, 0)
+
+    @lru_cache(None)
+    def dfs(self, target, pos):
+        if target < 0:
+            return False
+
+        if target == 0:
+            return True
+
+        if pos >= len(self.nums):
+            return False
+
+        return self.dfs(target - self.nums[pos], pos + 1) or self.dfs(target, pos + 1)
+
