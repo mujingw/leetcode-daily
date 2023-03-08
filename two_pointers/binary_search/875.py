35b0cab5488612165aced2dca31c1f31b8dfb975
mujingw <mqw5130@gmail.com>
2023-03-08 03:36:27 -0500

    [LC-875]

--- two_pointers/binary_search/875.py
+++ two_pointers/binary_search/875.py
@@ -3,24 +3,12 @@ from typing import List
 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def ok(k):
-            hours = 0
-
-            for pile in piles:
-                time_taken, leftover = divmod(pile, k)
-                hours += time_taken
-
-                if leftover > 0:
-                    hours += 1
-
-            return hours <= h
-
-        l, r = 1, 10 ** 13 + 1
+        l, r = 1, 10 ** 13
 
         while l < r:
             mid = (l + r) // 2
 
-            if ok(mid):
+            if sum(ceil(p / mid) for p in piles) <= h:
                 r = mid
             else:
                 l = mid + 1
