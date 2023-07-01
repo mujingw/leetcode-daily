3087d9a44dc7b4943a0180a11c887a44778add8c
mujingw <mqw5130@gmail.com>
2023-06-30 20:08:08 -0400

    [LC-678]

--- parentheses/678.py
+++ parentheses/678.py
@@ -1,23 +1,17 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        max_right_allowed = 0
-        min_right_needed = 0
+        return self.ok('(', s) and self.ok(')', s[::-1])
+
+    def ok(self, opening_chr, s):
+        count = 0
 
         for ch in s:
-            if ch == '(':
-                max_right_allowed += 1
-                min_right_needed += 1
-            elif ch == ')':
-                max_right_allowed -= 1
-                min_right_needed -= 1
+            if ch == opening_chr or ch == '*':
+                count += 1
             else:
-                max_right_allowed += 1
-                min_right_needed -= 1
+                count -= 1
 
-            if max_right_allowed < 0:
+            if count < 0:
                 return False
 
-            if min_right_needed < 0:
-                min_right_needed = 0
-
-        return min_right_needed == 0
+        return True
