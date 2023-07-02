9bae3f0b61d80b81afcb690b7626185c7329f752
mujingw <mqw5130@gmail.com>
2023-07-01 20:18:18 -0400

    [LC-714]

--- dynamic_programming/bottom_up/714.py
+++ dynamic_programming/bottom_up/714.py
@@ -4,15 +4,12 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         N = len(prices)
-        dp = [[float('-inf') for _ in range(2)] for _ in range(N)]
-        dp[0][0] = 0
-        dp[0][1] = -prices[0] - fee
+        hold = [0] * N
+        not_hold = [0] * N
+        hold[0] = -prices[0] - fee
 
-        for day in range(1, N):
-            for hold_stock in (0, 1):
-                if hold_stock == 0:
-                    dp[day][hold_stock] = max(dp[day - 1][0], dp[day - 1][1] + prices[day])
-                else:
-                    dp[day][hold_stock] = max(dp[day - 1][1], dp[day - 1][0] - prices[day] - fee)
+        for i in range(1, N):
+            hold[i] = max(not_hold[i-1] - prices[i] - fee, hold[i-1])
+            not_hold[i] = max(not_hold[i-1], hold[i-1] + prices[i])
 
-        return max(dp[N - 1])
+        return max(hold[-1], not_hold[-1])
