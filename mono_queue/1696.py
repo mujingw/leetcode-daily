d70430289b79875c9fa6d83710101190469e2ca7
mujingw <mqw5130@gmail.com>
2023-07-07 19:03:57 -0400

    [LC-1696]

--- mono_queue/1696.py
+++ mono_queue/1696.py
@@ -5,18 +5,18 @@ from typing import List
 class Solution:
     def maxResult(self, nums: List[int], k: int) -> int:
         q = deque([0])
-        res = [0] * len(nums)
+        dp = [0] * len(nums)
 
         for i, v in enumerate(nums):
             while q and i - q[0] > k:
                 q.popleft()
 
             if q:
-                res[i] = v + res[q[0]]
+                dp[i] = v + dp[q[0]]
 
-            while q and res[q[-1]] < res[i]:
+            while q and dp[q[-1]] < dp[i]:
                 q.pop()
 
             q.append(i)
 
-        return res[-1]
+        return dp[-1]
