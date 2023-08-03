db145b656b1e6e39edfbd767415594deccc1920f
mujingw <mqw5130@gmail.com>
2023-08-02 20:35:34 -0400

    [LC-1218]

---
+++ dynamic_programming/bottom_up/1218.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def longestSubsequence(self, arr: List[int], difference: int) -> int:
+        dp = defaultdict(int)
+
+        for v in arr:
+            dp[v] = dp[v - difference] + 1
+
+        return max(dp.values())
