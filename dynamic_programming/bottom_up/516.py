73964f6dc845e2ed79e1d846eabaae487ce6a3ab
mujingw <mqw5130@gmail.com>
2023-08-11 20:50:16 -0400

    [LC-516]

--- dynamic_programming/bottom_up/516.py
+++ dynamic_programming/bottom_up/516.py
@@ -1,19 +1,19 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        if s == s[::-1]:
-            return len(s)
-
         N = len(s)
-        dp = [[0 for _ in range(N)] for _ in range(N)]
+        dp = [[1 for _ in range(N)] for _ in range(N)]
 
-        for i in range(N):
-            dp[i][i] = 1
+        for i in range(N - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = 2
 
-        for i in range(N - 1, -1, -1):
-            for j in range(i + 1, N):
-                if s[i] == s[j]:
-                    dp[i][j] = dp[i + 1][j - 1] + 2
+        for size in range(3, N + 1):
+            for start in range(N - size + 1):
+                end = start + size - 1
+
+                if s[start] == s[end]:
+                    dp[start][end] = dp[start + 1][end - 1] + 2
                 else:
-                    dp[i][j] = max(dp[i][j - 1], dp[i + 1][j])
+                    dp[start][end] = max(dp[start + 1][end], dp[start][end - 1])
 
         return dp[0][-1]
