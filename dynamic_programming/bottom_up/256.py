2e51bd7e77e6b993efb6c5d30d303d0d64c2195f
mujingw <mqw5130@gmail.com>
2023-08-15 00:18:46 -0400

    [LC-256]

--- dynamic_programming/bottom_up/256.py
+++ dynamic_programming/bottom_up/256.py
@@ -3,17 +3,15 @@ from typing import List
 
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
+        R, B, G = 0, 1, 2
         N = len(costs)
-        red = [float('inf')] * N
-        blue = [float('inf')] * N
-        green = [float('inf')] * N
-        red[0] = costs[0][0]
-        blue[0] = costs[0][1]
-        green[0] = costs[0][2]
+        red = [costs[0][R]] * N
+        blue = [costs[0][B]] * N
+        green = [costs[0][G]] * N
 
         for i in range(1, N):
-            red[i] = min(blue[i-1], green[i-1]) + costs[i][0]
-            blue[i] = min(red[i-1], green[i-1]) + costs[i][1]
-            green[i] = min(blue[i-1], red[i-1]) + costs[i][2]
+            red[i] = min(blue[i-1], green[i-1]) + costs[i][R]
+            blue[i] = min(red[i-1], green[i-1]) + costs[i][B]
+            green[i] = min(red[i-1], blue[i-1]) + costs[i][G]
 
-        return int(min(red[-1], blue[-1], green[-1]))
+        return min(red[-1], blue[-1], green[-1])
