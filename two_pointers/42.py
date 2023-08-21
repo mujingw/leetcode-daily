1dcfed0e926e7839a285d24ec29e41ef81d9a2d6
mujingw <mqw5130@gmail.com>
2023-08-21 03:37:04 -0400

    [LC-42]

--- two_pointers/42.py
+++ two_pointers/42.py
@@ -3,19 +3,19 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        res = 0
-        lmax, rmax = 0, 0
         l, r = 0, len(height) - 1
+        res, lmax, rmax = 0, height[0], height[r]
 
         while l < r:
-            lmax = max(lmax, height[l])
-            rmax = max(rmax, height[r])
-
             if lmax < rmax:
-                res += (lmax - height[l])
+                res += lmax - height[l]
                 l += 1
+                lmax = max(lmax, height[l])
             else:
-                res += (rmax - height[r])
+                res += rmax - height[r]
                 r -= 1
+                rmax = max(rmax, height[r])
+
+        res += (max(min(lmax, rmax), 0)  - height[l])
 
         return res
