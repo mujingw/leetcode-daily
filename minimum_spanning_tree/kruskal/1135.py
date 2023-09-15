84d701d4f8d40084d89bba4103e5cfe0a2f4f54c
mujingw <mqw5130@gmail.com>
2023-09-15 00:43:31 -0400

    [LC-1135]

---
+++ minimum_spanning_tree/kruskal/1135.py
@@ -0,0 +1,75 @@
+from heapq import heappop, heappush
+from typing import List
+
+
+class UF:
+
+    def __init__(self):
+        self.parent = {}
+        self.rank = {}
+
+    def add(self, x):
+        if x in self.parent:
+            return
+
+        self.parent[x] = x
+        self.rank[x] = 1
+
+    def union(self, u, v):
+        ru, rv = self.find(u), self.find(v)
+
+        if ru == rv:
+            return
+
+        if self.rank[ru] < self.rank[rv]:
+            self.parent[ru] = rv
+        elif self.rank[rv] < self.rank[ru]:
+            self.parent[rv] = ru
+        else:
+            self.parent[ru] = rv
+            self.rank[rv] += 1
+
+    def find(self, x):
+        if self.parent[x] == x:
+            return x
+
+        self.parent[x] = self.find(self.parent[x])
+
+        return self.parent[x]
+
+
+class Edge:
+
+    def __init__(self, u, v, w):
+        self.u = u
+        self.v = v
+        self.w = w
+
+    def __lt__(self, other):
+        return self.w < other.w
+
+
+class Solution:
+    def minimumCost(self, N: int, connections: List[List[int]]) -> int:
+        edge_count = 0
+        cost = 0
+        uf = UF()
+        h = []
+
+        for u, v, w in connections:
+            heappush(h, Edge(u, v, w))
+
+        while edge_count < N - 1 and h:
+            e = heappop(h)
+            u, v, w = e.u, e.v, e.w
+            uf.add(u)
+            uf.add(v)
+
+            if uf.find(u) == uf.find(v):
+                continue
+
+            edge_count += 1
+            uf.union(u, v)
+            cost += w
+
+        return cost if edge_count == N - 1 else -1
